dc627bd3e5423774f13cb8008290ea1cc4955d20
Arthur Sobreira <tutusobreira@gmail.com>
2023-09-26 10:13:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:34:59 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/09/25 19:46:54 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/09/26 10:10:58 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/fdf.h"
+
+#define MLX_ERROR 1
 
 typedef struct s_data
 {
@@ -22,19 +24,21 @@ int main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	void	*img_ptr;
-	int		color = 0xFF0000;
 
 	mlx_ptr = mlx_init();
+	// Error verification
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
+
 	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "fdf muito pika");
-	img_ptr = mlx_new_image(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	
-	mlx_pixel_put(mlx_ptr, img_ptr, 400, 300, color);
-
-	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
-
-	mlx_loop(mlx_ptr);
-
+	// Error verification
+	if (win_ptr == NULL)
+	{
+		free(win_ptr);
+		return (MLX_ERROR);
+	}
+	while (1)
+		;
 	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
