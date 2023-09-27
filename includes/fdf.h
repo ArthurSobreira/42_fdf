8d9ce75ae01b0b92054a0d2285dd722dddf258de
Arthur Sobreira <tutusobreira@gmail.com>
2023-09-27 19:15:24 -0300

    Update fdf header

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,17 +6,17 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 09:15:50 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/09/25 16:52:11 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/09/27 19:11:48 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
-#define FDF_H
+# define FDF_H
 
-#include "../minilibx/mlx.h"
-#include "../libft/libft.h"
+# include "../libft/libft.h"
+# include "../minilibx/mlx.h"
 
-#define WINDOW_WIDTH 900
-#define WINDOW_HEIGHT 600
+# define WINDOW_WIDTH 900
+# define WINDOW_HEIGHT 600
 
 #endif
