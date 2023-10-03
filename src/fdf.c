9269c0e5f371a34bc5bdb30c32090a0fdfe53bde
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-03 12:15:04 -0300

    Update fdf.c

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 10:14:19 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/02 11:44:26 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/03 12:14:11 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,20 @@
 #define BLUE_PIXEL 0x0000ff
 #define GREEN_PIXEL 0x00ff00
 
+typedef struct s_image
+{
+	void	*mlx_image;
+	char	*adress;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_image;
+
 typedef struct s_data
 {
     void    *mlx_ptr;
     void    *win_ptr;
+	t_image	img;
 }           t_data;
 
 typedef	struct s_rect
@@ -32,7 +42,15 @@ typedef	struct s_rect
     int color;
 }		t_rect;
 
-int	render_x(t_data *data, int iter)
+void	pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->img.adress + (y * data->img.line_length + x * (data->img.bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	render_x(t_data *data, int iter, int color)
 {
 	int	index;
 
@@ -40,23 +58,23 @@ int	render_x(t_data *data, int iter)
 	if (data->win_ptr != NULL)
 	{
 		index = 0;
-		while (index <= iter)
-		{
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2) + index, (WINDOW_HEIGHT / 2) + index, (BLUE_PIXEL * (index + 10)));
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2) + index, (WINDOW_HEIGHT / 2) - index, (BLUE_PIXEL * (index + 10)));
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2) - index, (WINDOW_HEIGHT / 2) - index, (BLUE_PIXEL * (index + 10)));
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2) - index, (WINDOW_HEIGHT / 2) + index, (BLUE_PIXEL * (index + 10)));
-			index++;
-		}
+		while (index++ <= iter)
+			pixel_put(data, ((WINDOW_WIDTH / 2) + index), ((WINDOW_HEIGHT / 2) + index), (color * (index + 10)));
+		index = 0;
+		while (index++ <= iter)
+			pixel_put(data, ((WINDOW_WIDTH / 2) - index), ((WINDOW_HEIGHT / 2) + index), (color * (index + 10)));		
+		index = 0;
+		while (index++ <= iter)
+			pixel_put(data, ((WINDOW_WIDTH / 2) + index), ((WINDOW_HEIGHT / 2) - index), (color * (index + 10)));
+		index = 0;
+		while (index++ <= iter)
+			pixel_put(data, ((WINDOW_WIDTH / 2) - index), ((WINDOW_HEIGHT / 2) - index), (color * (index + 10)));			
 	}
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_image, 0, 0);
 	return (0);
 }
 
-int	render_plus(t_data *data, int iter)
+int	render_plus(t_data *data, int iter, int color)
 {
 	int	index;
 
@@ -64,19 +82,19 @@ int	render_plus(t_data *data, int iter)
 	if (data->win_ptr != NULL)
 	{
 		index = 0;
-		while (index <= iter)
-		{
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2) + index, (WINDOW_HEIGHT / 2), (BLUE_PIXEL * (index + 10)));
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2) - index, (WINDOW_HEIGHT / 2), (BLUE_PIXEL * (index + 10)));
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2) + index, (BLUE_PIXEL * (index + 10)));
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, \
-			(WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2) - index, (BLUE_PIXEL * (index + 10)));			
-			index++;
-		}
+		while (index++ <= iter)
+			pixel_put(data, ((WINDOW_WIDTH / 2) + index), (WINDOW_HEIGHT / 2), (color * (index + 10)));
+		index = 0;
+		while (index++ <= iter)
+			pixel_put(data, ((WINDOW_WIDTH / 2) - index), (WINDOW_HEIGHT / 2), (color * (index + 10)));		
+		index = 0;
+		while (index++ <= iter)
+			pixel_put(data, (WINDOW_WIDTH / 2), ((WINDOW_HEIGHT / 2) + index), (color * (index + 10)));
+		index = 0;
+		while (index++ <= iter)
+			pixel_put(data, (WINDOW_WIDTH / 2), ((WINDOW_HEIGHT / 2) - index), (color * (index + 10)));			
 	}
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_image, 0, 0);
 	return (0);
 }
 
@@ -94,11 +112,12 @@ int	render_rect(t_data *data, t_rect rect)
         j = rect.x;
         while (j < rect.x + rect.width)
 		{
-            mlx_pixel_put(data->mlx_ptr, data->win_ptr, j, i, (rect.color * j));
+            pixel_put(data, j, i, (rect.color * j));
 			j++;
 		}
         ++i;
     }
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_image, 0, 0);
 	return (0);
 }
 
@@ -116,39 +135,66 @@ void	render_background(t_data *data, int color)
 			x = 0;
 			while (x < WINDOW_WIDTH)
 			{
-				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, color);
+				pixel_put(data, x, y, color);
 				x++;
 			}
 			++y;
 		}
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_image, 0, 0);
+	}	
+}
+
+void	clear_window(t_data *data)
+{
+	int	x;
+	int	y;
+	
+	// Verify if window has been destroyed
+	if (data->win_ptr != NULL)
+	{	
+		y = 0;
+		while (y < WINDOW_HEIGHT)
+		{
+			x = 0;
+			while (x < WINDOW_WIDTH)
+			{
+				pixel_put(data, x, y, 0x000000);
+				x++;
+			}
+			++y;
+		}
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_image, 0, 0);
 	}	
 }
 
 int	handle_keypress(int key, t_data *data)
 {
 	// Clear the window if there is anything on it
-	mlx_clear_window(data->mlx_ptr, data->win_ptr);
+	clear_window(data);
 
 	if (key == XK_Escape)
 		mlx_loop_end(data->mlx_ptr);
 	else if (key == XK_Up)
-		render_x(data, 200);
+		render_x(data, 200, RED_PIXEL);
 	else if (key == XK_Down)
-		render_x(data, 100);
+		render_x(data, 100, RED_PIXEL);
 	else if (key == XK_Right)
-		render_plus(data, 200);
+		render_plus(data, 200, RED_PIXEL);
 	else if (key == XK_Left)
-		render_plus(data, 100);
+		render_plus(data, 100, RED_PIXEL);
 	else if (key == XK_1)
-		render_background(data, 0x0000ff);
+		render_background(data, 0x00ff00);
 	else if (key == XK_2)
 		render_rect(data, (t_rect){((WINDOW_WIDTH / 2) - 75), ((WINDOW_HEIGHT / 2) - 75),
 		120, 120, RED_PIXEL});
+	else
+		return (1);
 	return (0);
 }
 
 void	clear_all(t_data *data)
 {
+	mlx_destroy_image(data->mlx_ptr, data->img.mlx_image);
 	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 	mlx_destroy_display(data->mlx_ptr);
 	data->win_ptr = NULL;
@@ -160,11 +206,13 @@ int main(void)
 	t_data	data;
 
 	data.mlx_ptr = mlx_init();
+	
 	// Error Verification
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
 	
 	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "fdf - 42");
+	
 	// Error Verification
 	if (data.win_ptr == NULL)
 	{
@@ -172,6 +220,17 @@ int main(void)
 		return (MLX_ERROR);
 	}
 	
+	// Create the image
+	data.img.mlx_image = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	/*
+	** After creating an image, we can call `mlx_get_data_addr`, we pass
+	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
+	** then be set accordingly for the *current* data address.
+	*/
+	data.img.adress = mlx_get_data_addr(data.img.mlx_image, &data.img.bits_per_pixel, \
+										&data.img.line_length, &data.img.endian);
+
 	// Hook for keypress
 	mlx_key_hook(data.win_ptr, &handle_keypress, &data);
 
