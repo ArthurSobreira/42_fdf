301aae0edf21ed11645968bfbd904d1ddf8f1603
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-04 18:40:51 -0300

    Update fdf header

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 09:15:50 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/04 10:46:08 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/04 18:40:27 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,13 @@
 # define WINDOW_WIDTH 900
 # define WINDOW_HEIGHT 600
 # define WINDOW_NAME "FDF - 42"
+
+typedef struct s_point
+{
+	float	x;
+	float	y;
+	float	z;
+}			t_point;
 
 typedef struct s_img
 {
@@ -44,5 +51,6 @@ void	pixel_put(t_fdf *fdf, int x, int y, int color);
 int		handle_keypress(int key, t_fdf *fdf);
 void	clear_all(t_fdf *fdf);
 void	render_background(t_fdf *fdf, int color);
+void	bresenham(t_fdf *fdf, t_point inital_point, t_point end_point);
 
 #endif
