5b638fb2dc7c53ecd652b6eef6931f40435a2b11
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-05 12:51:41 -0300

    Update libft/Makefile

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 11:17:07 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/04 18:26:28 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/05 10:20:32 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	bresenham(t_fdf *fdf, t_point inital_point, t_point end_point)
 	e = 0;
 	while (x <= end_point.x)
 	{
-		pixel_put(fdf, x, y, (0x00ff00 * (x / 2)));
+		pixel_put(fdf, x, y, (0x0000ff * (x / 4)));
 		x = x + 1;
 		e = e + (2 * delta_y);
 		if (e >= delta_x)
@@ -38,4 +38,13 @@ void	bresenham(t_fdf *fdf, t_point inital_point, t_point end_point)
 		}
 	}
 	mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, fdf->img.mlx_image, 0, 0);
+}
+
+void	pixel_put(t_fdf *fdf, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = fdf->img.adress + (y * fdf->img.line_length + x \
+								* (fdf->img.bits_per_pixel / 8));
+	*(unsigned int *)pixel = color;
 }
