a7c052fc247bdb49161645b7b0df887aafd398e9
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-06 14:20:42 -0300

    Update handle_keys.c

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:41:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/04 18:41:58 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:40:50 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,9 @@
 
 int	handle_keypress(int key, t_fdf *fdf)
 {
+	render_background(fdf, 0x1a1a1a);
+	
 	if (key == XK_Escape)
 		mlx_loop_end(fdf->mlx_ptr);
-	else if (key == XK_1)
-		bresenham(fdf, (t_point){20, 20, 0}, (t_point){500, 100, 0});
-	else if (key == XK_2)
-		bresenham(fdf, (t_point){20, 20, 0}, (t_point){500, 200, 0});
-	else if (key == XK_3)
-		bresenham(fdf, (t_point){20, 20, 0}, (t_point){500, 300, 0});
-	else if (key == XK_4)
-		bresenham(fdf, (t_point){20, 20, 0}, (t_point){500, 400, 0});
-	else if (key == XK_5)
-		bresenham(fdf, (t_point){20, 20, 0}, (t_point){500, 500, 0});
-	else if (key == XK_6)
-		bresenham(fdf, (t_point){200, 20, 0}, (t_point){50, 50, 0});
 	return (0);
 }
