b5fd6c8d637b09e4a73958ca02876a01372854e2
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-10 20:08:56 -0300

    Update read_map.c

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:12:37 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 19:06:29 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/10 20:06:47 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,48 @@ t_map	*read_map(char *map_name)
 		handle_error(7);
 	map->width = get_width(map_name);
 	map->height = get_height(map_name);
+	ft_printf("width: %d\n", map->width);
+	ft_printf("height: %d\n", map->height);
 	// map->matrix = get_matrix(map_name, map->x_max, map->y_max);
 	return (map);
 }
 
 static int	get_width(char *map_name)
 {
-	(void)map_name;
-	return (0);
+	int		file_descriptor;
+	int		width;
+	int		index;
+	char	*line;
+	char	**split_line;
+
+	file_descriptor = open(map_name, O_RDONLY);
+	width = 0;
+	while (1)
+	{
+		line = get_next_line(file_descriptor);
+		if ((line == NULL) && (!ft_isprint(*line)))
+			break;
+		split_line = ft_split(line, ' ');
+		index = 0;
+		while (split_line[index] != NULL)
+		{
+			if ((line == NULL) && (index == 0))
+				width++;
+			free(split_line[index]);
+			index++;
+		}
+		free(split_line);
+		free(line);
+	}
+	close(file_descriptor);
+	return (width);
 }
 
 static int	get_height(char *map_name)
 {
 	int		file_descriptor;
-	char	*line;
 	int		height;
+	char	*line;
 
 	file_descriptor = open(map_name, O_RDONLY);
 	height = 0;
