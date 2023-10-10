e4691b741be254b2acecd39fc2cff1edc6686da9
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-10 17:13:32 -0300

    Update init.c

--- src/init.c
+++ src/init.c
@@ -6,11 +6,51 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:31:22 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 10:35:29 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/10 17:05:34 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+t_fdf	*init_fdf(char *map_name)
+{
+	t_fdf	*fdf;
+
+	fdf = (t_fdf *)malloc(sizeof(t_fdf));
+	if (fdf == NULL)
+		handle_error(2);
+	fdf->mlx_ptr = mlx_init();
+	if (fdf->mlx_ptr == NULL)
+		handle_error(3);
+	fdf->win_ptr = mlx_new_window(fdf->mlx_ptr, \
+							WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
+	if (fdf->win_ptr == NULL)
+	{
+		free(fdf->win_ptr);
+		handle_error(4);
+	}
+	fdf->img.mlx_image = mlx_new_image(fdf->mlx_ptr, WINDOW_WIDTH, \
+														WINDOW_HEIGHT);
+	fdf->img.adress = mlx_get_data_addr(fdf->img.mlx_image, \
+			&fdf->img.bits_per_pixel, &fdf->img.line_length, &fdf->img.endian);
+	fdf->map = read_map(map_name);
+	if (fdf->map == NULL)
+		handle_error(7);
+	return (fdf);
+}
+
+t_map	*init_map(void)
+{
+	t_map	*map;
+
+	map = (t_map *)malloc(sizeof(t_map));
+	if (map == NULL)
+		handle_error(7);
+	map->matrix = NULL;
+	map->x_max = 0;
+	map->y_max = 0;
+	return (map);
+}
 
 void	init_bres(t_bres *bres_info, t_point initial_point, t_point end_point)
 {
