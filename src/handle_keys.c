c20c2279d5c699387673d4a07ad04cb015cc0aed
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-10 13:41:14 -0300

    Update handle_keys.c

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -6,19 +6,15 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:41:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 11:44:35 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/10 13:33:13 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	handle_keypress(t_fdf *fdf, int key)
+int	handle_keypress(int key, t_fdf *fdf)
 {
-	if (key == KEY_ESC || key == XK_Escape)
-	{
-		ft_printf("ESC pressed\n");
+	if (key == XK_Escape)
 		mlx_loop_end(fdf->mlx_ptr);
-		clear_all(fdf);
-	}
 	return (0);
 }
