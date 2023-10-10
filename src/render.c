f494e0fa0993d2ae8f5183cb5f3f10fe9089e056
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-10 13:40:28 -0300

    Update render.c

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:33:43 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 11:56:54 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/10 13:37:14 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,9 @@
 
 void	render(t_fdf *fdf)
 {
-	render_background(fdf, 0x1a1a1a);
-
-    int x1, y1, x2, y2;
-
-    int interval = 40;
-
-    for (x1 = 0; x1 < 900; x1 += interval)
-    {
-        for (y1 = 0; y1 < 600; y1 += interval)
-        {
-            for (x2 = 0; x2 < 900; x2 += interval)
-            {
-                for (y2 = 0; y2 < 600; y2 += interval)
-                {
-                    bresenham(fdf, (t_point){x1, y1, 0, 0xff}, (t_point){x2, y2, 0, 0xff});
-                }
-                mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, \
+	render_background(fdf, WINDOW_BACKGROUND);
+	mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, \
 								fdf->img.mlx_image, 0, 0);
-            }
-        }
-    }
 }
 
 void	render_background(t_fdf *fdf, int color)
@@ -51,7 +33,5 @@ void	render_background(t_fdf *fdf, int color)
 			while (x++ < WINDOW_WIDTH)
 				pixel_put(fdf, x, y, color);
 		}
-		mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, \
-								fdf->img.mlx_image, 0, 0);
 	}
 }
