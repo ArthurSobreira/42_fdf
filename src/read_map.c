7c4973587e895f13c1c10d1c3ff881d57d09cebb
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-10 19:09:01 -0300

    Update read_map.c

--- src/read_map.c
+++ src/read_map.c
@@ -6,13 +6,15 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:12:37 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 17:45:29 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/10 19:06:29 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static short	valid_map(char *file_name);
+static int	valid_map(char *file_name);
+static int	get_width(char *map_name);
+static int	get_height(char *map_name);
 
 t_map	*read_map(char *map_name)
 {
@@ -21,10 +23,41 @@ t_map	*read_map(char *map_name)
 	if (!valid_map(map_name))
 		handle_error(1);
 	map = init_map();
+	if (map == NULL)
+		handle_error(7);
+	map->width = get_width(map_name);
+	map->height = get_height(map_name);
+	// map->matrix = get_matrix(map_name, map->x_max, map->y_max);
 	return (map);
 }
 
-static short	valid_map(char *file_name)
+static int	get_width(char *map_name)
+{
+	(void)map_name;
+	return (0);
+}
+
+static int	get_height(char *map_name)
+{
+	int		file_descriptor;
+	char	*line;
+	int		height;
+
+	file_descriptor = open(map_name, O_RDONLY);
+	height = 0;
+	while (1)
+	{
+		line = get_next_line(file_descriptor);
+		if ((line == NULL) && (!ft_isprint(*line)))
+			break;
+		height++;
+		free(line);	
+	}
+	close(file_descriptor);
+	return (height);
+}
+
+static int	valid_map(char *file_name)
 {
 	int	file_descriptor;
 
