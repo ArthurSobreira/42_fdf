cf9ad92eb8a87a7779d88ebb6bb210cf66497b0b
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-11 15:56:29 -0300

    Update read_utils.c

--- src/read_utils.c
+++ src/read_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   read_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 09:23:42 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/11 09:29:18 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:55:37 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,18 @@ void	free_split(char **split_line)
 		index++;
 	}
 	free(split_line);
+}
+
+int	valid_map_name(char *file_name)
+{
+	int	file_descriptor;
+
+	file_descriptor = open(file_name, O_RDONLY);
+	if (file_descriptor < 0)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+	close(file_descriptor);
+	return (1);
 }
