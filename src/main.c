65329e6699de034ba102cef03e5f586e6241a44b
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-12 16:46:50 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:00:26 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 17:09:41 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:32:03 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ int	main(int argc, char *argv[])
 	char	*map_name;
 	t_fdf	*fdf;
 
-	if (argc != 2)
-		handle_error(1);
 	map_name = argv[1];
+	if ((argc != 2) || (!valid_map_name(map_name)))
+		handle_error(1);
 	fdf = init_fdf(map_name);
 	render(fdf);
 	mlx_key_hook(fdf->win_ptr, &handle_keypress, fdf);
 	mlx_hook(fdf->win_ptr, DestroyNotify, KeyReleaseMask, mlx_loop_end,
 		fdf->mlx_ptr);
 	mlx_loop(fdf->mlx_ptr);
-	clear_all(fdf);
+	clear_all(fdf, 0);
 	return (0);
 }
