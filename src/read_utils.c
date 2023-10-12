d78ec976cf0a8ebef89bf5fb2f2462ff3ae726f0
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-12 19:28:05 -0300

    Update read_map.c

--- src/read_utils.c
+++ src/read_utils.c
@@ -6,11 +6,40 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 09:23:42 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/12 18:54:50 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/12 19:23:18 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+t_point	**init_matrix(int width, int height)
+{
+	t_point	**matrix;
+	int		i;
+	int		j;
+
+	matrix = malloc(width * sizeof(t_point *));
+	if (matrix == NULL)
+		handle_error(8);
+	i = 0;
+	while (i < width)
+	{
+		matrix[i] = malloc(height * sizeof(t_point));
+		if (matrix[i] == NULL)
+			handle_error(8);
+		j = 0;
+		while (j < height)
+		{
+			matrix[i][j].x = 0;
+			matrix[i][j].y = 0;
+			matrix[i][j].z = 0;
+			matrix[i][j].color = 0;
+			j++;
+		}
+		i++;
+	}
+	return (matrix);
+}
 
 short	valid_map_name(char *file_name)
 {
