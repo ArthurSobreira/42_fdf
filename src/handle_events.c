a7906e5a3a1ea264a769a48f1b40193f4bd662a4
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-12 18:43:43 -0300

    Update handle_events.c

--- src/handle_events.c
+++ src/handle_events.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:41:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/12 15:35:01 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/12 18:18:55 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	clear_all(t_fdf *fdf, short exit_code)
 	mlx_destroy_window(fdf->mlx_ptr, fdf->win_ptr);
 	mlx_destroy_display(fdf->mlx_ptr);
 	free(fdf->mlx_ptr);
+	free(fdf->map->matrix);
 	free(fdf->map);
 	free(fdf);
 	handle_error(exit_code);
