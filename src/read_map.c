1faa71a9100cb0d27a8200af9b819ae413a54c58
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-12 17:00:01 -0300

    Update read_map.c

--- src/read_map.c
+++ src/read_map.c
@@ -6,13 +6,12 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:12:37 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/10 20:06:47 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:58:25 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static int	valid_map(char *file_name);
 static int	get_width(char *map_name);
 static int	get_height(char *map_name);
 
@@ -20,55 +19,49 @@ t_map	*read_map(char *map_name)
 {
 	t_map	*map;
 
-	if (!valid_map(map_name))
-		handle_error(1);
 	map = init_map();
 	if (map == NULL)
 		handle_error(7);
-	map->width = get_width(map_name);
-	map->height = get_height(map_name);
-	ft_printf("width: %d\n", map->width);
-	ft_printf("height: %d\n", map->height);
+	map->width = get_width(map_name); // verificar se é maior que 2
+	map->height = get_height(map_name); // verificar se é maior que 2		
+	ft_printf("width: %d | height: %d\n", map->width, map->height);
+	//map->matrix = init_matrix();
 	// map->matrix = get_matrix(map_name, map->x_max, map->y_max);
 	return (map);
 }
 
 static int	get_width(char *map_name)
 {
+	char	*line;
+	char	**split_line;
 	int		file_descriptor;
 	int		width;
 	int		index;
-	char	*line;
-	char	**split_line;
 
 	file_descriptor = open(map_name, O_RDONLY);
+	line = get_next_line(file_descriptor);
+	split_line = ft_split(line, ' ');
 	width = 0;
-	while (1)
+	index = 0;
+	while (split_line[index])
 	{
-		line = get_next_line(file_descriptor);
-		if ((line == NULL) && (!ft_isprint(*line)))
-			break;
-		split_line = ft_split(line, ' ');
-		index = 0;
-		while (split_line[index] != NULL)
-		{
-			if ((line == NULL) && (index == 0))
-				width++;
-			free(split_line[index]);
-			index++;
-		}
-		free(split_line);
-		free(line);
+		if (ft_strncmp(split_line[index], "\n",\
+					ft_strlen(split_line[index])))
+			width++;
+		index++;
 	}
-	close(file_descriptor);
+	free_split(split_line);
+	free(line);
+	if (!valid_width(file_descriptor, width))
+		return (FALSE);
 	return (width);
 }
 
 static int	get_height(char *map_name)
 {
 	int		file_descriptor;
-	int		height;
 	char	*line;
+	int		height;
 
 	file_descriptor = open(map_name, O_RDONLY);
 	height = 0;
@@ -82,18 +75,4 @@ static int	get_height(char *map_name)
 	}
 	close(file_descriptor);
 	return (height);
-}
-
-static int	valid_map(char *file_name)
-{
-	int	file_descriptor;
-
-	file_descriptor = open(file_name, O_RDONLY);
-	if (file_descriptor < 0)
-	{
-		close(file_descriptor);
-		return (0);
-	}
-	close(file_descriptor);
-	return (1);
 }
