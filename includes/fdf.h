610031cfb6129141d425c0db762e3c6c59c10893
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-13 11:51:52 -0300

    Update fdf header

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,15 +6,15 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 09:15:50 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/13 10:44:36 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:48:26 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include "../libft/libft.h"
-# include "../minilibx/mlx.h"
+# include "../libs/libft/libft.h"
+# include "../libs/minilibx/mlx.h"
 # include <fcntl.h>
 # include <stdlib.h>
 # include <math.h>
