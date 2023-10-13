423be894625b57fad614789c001f83f5b7717dbb
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-13 20:00:00 -0300

    Update render.c

--- src/render.c
+++ src/render.c
@@ -6,17 +6,47 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:33:43 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/13 16:30:44 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/13 19:47:29 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	line_render(t_fdf *fdf, t_point initial_point, t_point end_point);
+
 void	render(t_fdf *fdf)
 {
+	int	x;
+	int	y;
+
 	render_background(fdf, WINDOW_BACKGROUND);
+	y = 0;
+	while (y < fdf->map->height)
+	{
+		x = 0;
+		while (x < fdf->map->width)
+		{
+			if (x < fdf->map->width - 1)
+				line_render(fdf, fdf->map->matrix[y][x], \
+								fdf->map->matrix[y][x + 1]);
+			if (y < fdf->map->height - 1)
+				line_render(fdf, fdf->map->matrix[y][x], \
+								fdf->map->matrix[y + 1][x]);
+			x++;
+		}
+		y++;
+	}
 	mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, \
 								fdf->img.mlx_image, 0, 0);
+}
+
+static void	line_render(t_fdf *fdf, t_point initial_point, t_point end_point)
+{
+	initial_point.x *= fdf->cam->scale;
+	initial_point.y *= fdf->cam->scale;
+	end_point.x *= fdf->cam->scale;
+	end_point.y *= fdf->cam->scale;
+	bresenham(fdf, initial_point, end_point);
 }
 
 void	render_background(t_fdf *fdf, int color)
