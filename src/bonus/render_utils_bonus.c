82d5e39db9ab0cdf2701a0619725ae87fde77be8
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-16 17:45:09 -0300

    Update render_utils_bonus.c

--- src/bonus/render_utils_bonus.c
+++ src/bonus/render_utils_bonus.c
@@ -6,30 +6,47 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:41:18 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/16 17:11:24 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/16 17:43:00 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf_bonus.h"
 
-void	render_menu(t_fdf *fdf)
+void	render_menu(t_fdf *fdf, int color)
 {
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					25, 20, 0xffffff, "     __________  ______");
+					25, 20, color, "     __________  ______");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					25, 30, 0xffffff, "    / ____/ __ \\/ ____/");
+					25, 30, color, "    / ____/ __ \\/ ____/");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					25, 40, 0xffffff, "   / /_  / / / / /_    ");
+					25, 40, color, "   / /_  / / / / /_    ");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					25, 50, 0xffffff, "  / __/ / /_/ / __/    ");
+					25, 50, color, "  / __/ / /_/ / __/    ");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					25, 60, 0xffffff, " /_/   /_____/_/       ");
+					25, 60, color, " /_/   /_____/_/       ");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					20, 75, 0xffffff, "_________________________");
+					20, 75, color, "_________________________");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					20, 100, 0xffffff, "Increase Scale Z - 'x'");
+					20, 100, color, "Increase Scale Z - 'x'");
 	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
-					20, 120, 0xffffff, "Decrease Scale Z - 'z'");
+					20, 120, color, "Decrease Scale Z - 'z'");
+	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
+					20, 140, color, "Zoom in - '+'");
+	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
+					20, 160, color, "Zoom out - '-'");
+	render_move_menu(fdf, color);
+}
+
+void	render_move_menu(t_fdf *fdf, int color)
+{
+	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
+					20, 180, color, "Move Up - 'Arrow Up'");
+	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
+					20, 200, color, "Move Down - 'Arrow Down'");
+	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
+					20, 220, color, "Move Left - 'Arrow Left'");
+	mlx_string_put(fdf->mlx_ptr, fdf->win_ptr, \
+					20, 240, color, "Move Right - 'Arrow Right'");
 }
 
 void	render_menu_bar(t_fdf *fdf, int color)
