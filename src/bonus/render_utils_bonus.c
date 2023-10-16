b97275307770eaf1c187907f53cf21319392b0f7
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-16 17:57:09 -0300

    Update render_utils_bonus.c

--- src/bonus/render_utils_bonus.c
+++ src/bonus/render_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:41:18 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/16 17:43:00 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/16 17:55:00 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,11 +81,19 @@ void	render_background(t_fdf *fdf, int color)
 	if (fdf->win_ptr != NULL)
 	{
 		y = 0;
-		while (y++ < WINDOW_HEIGHT)
+		while (y < WINDOW_HEIGHT)
 		{
-			x = 200;
-			while (x++ < WINDOW_WIDTH)
+			x = 0;
+			while (x < WINDOW_WIDTH)
+			{
 				pixel_put(fdf, x, y, color);
+				if (x < 5 || x > 1095 || y < 5 || y > 795)
+					pixel_put(fdf, x, y, 0x010101);
+				else
+					pixel_put(fdf, x, y, color);
+				x++;
+			}
+			y++;
 		}
 		mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, \
 								fdf->img.mlx_image, 0, 0);
