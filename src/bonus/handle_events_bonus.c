a36f48f9079868d6ad44cadd7d5cd059a819a286
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-16 19:02:32 -0300

    Update handle_events_bonus.c

--- src/bonus/handle_events_bonus.c
+++ src/bonus/handle_events_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:41:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/16 18:51:37 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/16 18:59:04 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ int	handle_keypress(int key, t_fdf *fdf)
 	else if (key == XK_z)
 		fdf->cam->multi_factor -= 0.2;
 	else if (key == XK_Up)
-		fdf->cam->y_offset += 10;
+		fdf->cam->y_offset += 5;
 	else if (key == XK_Down)
-		fdf->cam->y_offset -= 10;
+		fdf->cam->y_offset -= 5;
 	else if (key == XK_Left)
-		fdf->cam->x_offset += 10;
+		fdf->cam->x_offset += 5;
 	else if (key == XK_Right)
-		fdf->cam->x_offset -= 10;
+		fdf->cam->x_offset -= 5;
 	else if (key == XK_i)
 		fdf->cam->projection = "ISO";
 	else if (key == XK_t)
