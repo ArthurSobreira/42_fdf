7e20f94ce1ae3cd949fa597d4e9f000c1ee5a3d2
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-16 15:24:42 -0300

    Update render.c

---
+++ src/mandatory/render.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 19:33:43 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/10/16 14:17:59 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static void	line_render(t_fdf *fdf, t_point initial_point, t_point end_point);
+
+int	render(t_fdf *fdf)
+{
+	int	x;
+	int	y;
+
+	render_background(fdf, WINDOW_BACKGROUND);
+	y = 0;
+	while (y < fdf->map->height)
+	{
+		x = 0;
+		while (x < fdf->map->width)
+		{
+			if (x < fdf->map->width - 1)
+				line_render(fdf, fdf->map->matrix[y][x], \
+								fdf->map->matrix[y][x + 1]);
+			if (y < fdf->map->height - 1)
+				line_render(fdf, fdf->map->matrix[y][x], \
+								fdf->map->matrix[y + 1][x]);
+			x++;
+		}
+		y++;
+	}
+	render_menu_bar(fdf, MENU_BAR);
+	mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, \
+								fdf->img.mlx_image, 0, 0);
+	return (0);
+}
+
+static void	line_render(t_fdf *fdf, t_point initial_point, t_point end_point)
+{
+	scale_map(fdf, &initial_point, &end_point);
+	isometric(fdf, &initial_point, &end_point);
+	centralize(fdf, &initial_point, &end_point);
+	bresenham(fdf, initial_point, end_point);
+}
