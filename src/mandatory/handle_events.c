68a25081c01ffa7914b682bdb18c5ad0c9bd53a2
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-16 15:31:32 -0300

    Update handle_events.c

---
+++ src/mandatory/handle_events.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_events.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 19:41:14 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/10/16 15:30:41 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	handle_keypress(int key, t_fdf *fdf)
+{
+	if (key == XK_Escape)
+		clear_all(fdf, 0);
+	else if (key == XK_x)
+		fdf->cam->multi_factor += 0.2;
+	else if (key == XK_z)
+		fdf->cam->multi_factor -= 0.2;
+	return (0);
+}
