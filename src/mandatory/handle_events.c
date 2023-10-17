04328951238544aee518288fe7d68f4a82b052cc
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-17 13:26:26 -0300

    Update handle_events.c

--- src/mandatory/handle_events.c
+++ src/mandatory/handle_events.c
@@ -6,19 +6,41 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 19:41:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/16 16:45:58 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/17 13:20:16 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	handle_z_scale(int key, t_fdf *fdf);
+
 int	handle_keypress(int key, t_fdf *fdf)
 {
 	if (key == XK_Escape)
 		clear_all(fdf, 0);
-	else if (key == XK_x)
-		fdf->cam->multi_factor += 0.2;
-	else if (key == XK_z)
-		fdf->cam->multi_factor -= 0.2;
+	else if (key == XK_x || key == XK_z)
+		handle_z_scale(key, fdf);
 	return (0);
+}
+
+static void	handle_z_scale(int key, t_fdf *fdf)
+{
+	if (key == XK_x)
+	{
+		if (fdf->map->max_z > 300)
+			fdf->cam->multi_factor += 0.02;
+		else if (fdf->map->max_z < 20)
+			fdf->cam->multi_factor += 1;
+		else
+			fdf->cam->multi_factor += 0.2;		
+	}
+	else if (key == XK_z)
+	{
+		if (fdf->map->max_z > 300)
+			fdf->cam->multi_factor -= 0.02;
+		else if (fdf->map->max_z < 20)
+			fdf->cam->multi_factor -= 1;
+		else
+			fdf->cam->multi_factor -= 0.2;
+	}
 }
