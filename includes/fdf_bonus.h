f66e5929356a9a5a456902ee225dc35fbf38d4b5
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-17 14:50:09 -0300

    Update bonus header

--- includes/fdf_bonus.h
+++ includes/fdf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 09:15:50 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/17 10:41:46 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/17 14:45:06 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ typedef struct s_map
 	int		width;
 	int		height;
 	float	max_z;
+	int		has_color;
 }			t_map;
 
 typedef struct s_cam
