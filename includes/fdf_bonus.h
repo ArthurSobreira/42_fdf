d073d855963868cce4e19cf26a6080db4559a0f9
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-17 16:05:58 -0300

    Update bonus header

--- includes/fdf_bonus.h
+++ includes/fdf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 09:15:50 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/17 15:10:13 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/17 15:57:26 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,9 @@
 # define WINDOW_BACKGROUND 0x1a1a1a
 
 // Color Macros
-# define BOTTOM_COLOR 0x0a1647
-# define TOP_COLOR 0xe7e7e4
+# define TOP_COLOR 0x878EA2
+# define MID_COLOR 0x535C7C
+# define BOTTOM_COLOR 0x29345E
 # define MENU_TEXT_COLOR 0xffffff
 # define MENU_BAR_COLOR 0x525252
 
