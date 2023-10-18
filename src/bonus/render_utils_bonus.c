242a871c55acaaf6071a29a249ba5fe2fd1941ad
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-18 12:00:26 -0300

    Update render_utils_bonus.c

--- src/bonus/render_utils_bonus.c
+++ src/bonus/render_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:41:18 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/16 18:49:55 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/18 10:15:39 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	render_menu_bar(t_fdf *fdf, int color)
 			x = 0;
 			while (x < 200)
 			{
-				if (x < 5 || x > 195 || y < 5 || y > 795)
+				if (x < 5 || x > 195 || y < 5 || y > 895)
 					pixel_put(fdf, x, y, 0x010101);
 				else
 					pixel_put(fdf, x, y, color);
@@ -60,7 +60,7 @@ void	render_background(t_fdf *fdf, int color)
 			while (x < WINDOW_WIDTH)
 			{
 				pixel_put(fdf, x, y, color);
-				if (x < 5 || x > 1095 || y < 5 || y > 795)
+				if (x < 5 || x > 1295 || y < 5 || y > 895)
 					pixel_put(fdf, x, y, 0x010101);
 				else
 					pixel_put(fdf, x, y, color);
