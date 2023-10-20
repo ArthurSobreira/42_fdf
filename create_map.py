778c1383d65ef99d9c771494fde1665cd6248176
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-20 17:35:32 -0300

    Update create_map.py

--- create_map.py
+++ create_map.py
@@ -7,12 +7,18 @@ width, height = imagem.size
 
 image_matrix = np.array(imagem)
 
-relief_map = np.zeros((height, width))
+relief_map = []
 
 for i in range(height):
+    row = []
     for j in range(width):
         height = int(np.mean(image_matrix[i, j])) - 128
-        relief_map[i, j] = height
+        color_info = f'0x{image_matrix[i, j][0]:02x}{image_matrix[i, j][1]:02x}{image_matrix[i, j][2]:02x}'
+        point_info = f'{height},{color_info}'
+        row.append(point_info)
+    relief_map.append(row)
 
 save_image = input("Enter the name that the image will be saved: ")
-np.savetxt(save_image, relief_map, fmt='%d')
+with open(save_image, 'w') as file:
+    for row in relief_map:
+        file.write(" ".join(row) + '\n')
